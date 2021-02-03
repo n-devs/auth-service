e79a5383b0fa54f58cb4c8778f0ea30f4634957f
Nutthapat Piasangka <n.devs260340@gmail.com>
2021-02-04 03:28:02 +0700

    update

--- public/javascripts/auth_facebook/index.js
+++ public/javascripts/auth_facebook/index.js
@@ -3,7 +3,10 @@ const passport = require('passport');
 const arj = require('api-response-json');
 const firebase = require('../../../sdk-connect/firebase');
 const facebookServiceAccount = require('../../../config/facebook/ServiceAccountKey.json');
+const rapidapiServiceAccount = require('../../../config/rapidapi/ServiceAccountKey.json');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const jwt = require('jsonwebtoken');
+const unirest = require("unirest");
 const router = express.Router();
 
 // const facebook =require('../../../sdk-connect/facebook');
@@ -27,61 +30,8 @@ router.get('/example/facebook/profile', isLoggedIn, function (req, res) {
 
 router.get('/facebook/callback', isLoggedIn, async function (req, res) {
   // get the user out of session and pass to template
+  arj.ok(res, true, "ok", { ...req.user })
 
-  const facebookUser = firestore.collection("facebook").doc(req.user.id);
-  const mainUser = firestore.collection("users").doc();
-  const authCol = firestore.collection("authentication");
-  // const logCol = firestore.collection("_log");
-  await facebookUser.get().then(function (doc) {
-    // console.log("isAuthenticated",req.isAuthenticated());
-    // console.log("headers:",req.headers);
-    // console.log("_startTime:",req._startTime);
-    // console.log("_remoteAddress:",req._remoteAddress);
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-      arj.ok(res, true, "ok", { ...doc.data() })
-    } else {
-      // doc.data() will be undefined in this case
-      mainUser.set({
-        id: mainUser.id,
-        name: req.user.displayName,
-        facebook_id: req.user.id
-      }).then(function () {
-        facebookUser.set({
-          uid: mainUser.id,
-          ...req.user
-        }).then(function () {
-          let sessionID = authCol.doc(mainUser.id).collection("status").doc(req.sessionID);
-          let authLog = authCol.doc(mainUser.id).collection("_log").doc();
-          sessionID.set({
-            uid: mainUser.id,
-            session: req.sessionID,
-            provider: req.user.provider,
-            facebook_id: req.user.id,
-            login_on: true,
-            date: req._startTime,
-            remoteAddress: req._remoteAddress,
-            ...req.headers
-          });
-          authLog.set({
-            uid: mainUser.id,
-            session: req.sessionID,
-            provider: req.user.provider,
-            facebook_id: req.user.id,
-            status: "signup",
-            date: req._startTime,
-            remoteAddress: req._remoteAddress,
-            ...req.headers
-          });
-
-          arj.ok(res, true, "ok", { ...req.user })
-        })
-      })
-      console.log("No such document!");
-    }
-  }).catch(function (error) {
-    console.log("Error getting document:", error);
-  });
 });
 
 router.get('/example/facebook/error', isLoggedIn, function (req, res) {
@@ -94,26 +44,248 @@ router.get('/auth/facebook', passport.authenticate('facebook', {
 
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    successRedirect: '/api/facebook/callback',
+    successRedirect: '/api/example/facebook/profile',
     failureRedirect: '/api/example/facebook/error'
   }));
 
-router.get('/facebook/logout', function (req, res) {
-  req.logout();
-  res.redirect('/api/example/facebook');
+router.get('/facebook/logout', async function (req, res) {
+  const geoReq = unirest("GET", "http://gd.geobytes.com/GetCityDetails");
+
+  const facebookUser = firestore.collection("facebook").doc(req.user.id);
+  const userCollect = firestore.collection("users");
+  const authCollect = firestore.collection("authentication");
+
+  // console.log(req.user.id);
+  // const token = jwt.sign({ session: req.sessionID }, `${req._startAt[1]}`, { expiresIn: '3d' });
+  // const logCol = firestore.collection("_log");
+
+  await facebookUser.get().then(function (doc) {
+
+    geoReq.end(function (_geoRes) {
+      if (doc.exists) {
+
+        let userDoc = userCollect.doc(doc.data().uid);
+        userDoc.get().then(function (_userDoc) {
+ 
+          if (_userDoc.exists) {
+   
+            let sessionID = authCollect.doc(_userDoc.data().id).collection("status").doc(req.sessionID);
+            let authLog = authCollect.doc(_userDoc.data().id).collection("_log");
+
+            sessionID.delete()
+
+            authLog.doc(`signin-${req.sessionID}`).get().then(function (docAuthLog) {
+      
+              authLog.doc(`signout-${req.sessionID}`).set({
+                ...docAuthLog.data(), ["_endTime"]: req._startTime,
+                ["_endAt"]: req._startAt,
+              }).then(function () {
+                req.logout();
+
+                res.redirect('/api/example/facebook');
+              })
+            })
+
+          } else {
+            console.log("no uid");
+          }
+        })
+
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+
+  }).catch(function (error) {
+    console.log("Error getting document:", error);
+  });
+
+
 });
 
 
-function isLoggedIn(req, res, next) {
+async function isLoggedIn(req, res, next) {
+  console.log(req._startAt);
   if (req.isAuthenticated()) {
-    // console.log(req,req.isAuthenticated());
+    const geoReq = unirest("GET", "http://gd.geobytes.com/GetCityDetails");
 
-    // console.log("_peername:",req._peername());
-    // console.log("sessions:",req.sessions);
+    const facebookUser = firestore.collection("facebook").doc(req.user.id);
+    const userCollect = firestore.collection("users");
+    const authCollect = firestore.collection("authentication");
+
+    const token = jwt.sign({ session: req.sessionID }, `${req._startAt[1]}`, { expiresIn: '3d' });
+    // const logCol = firestore.collection("_log");
+
+    await facebookUser.get().then(function (doc) {
+      geoReq.end(function (_geoRes) {
+        if (doc.exists) {
+          let userDoc = userCollect.doc(doc.data().uid);
+          userDoc.get().then(function (_userDoc) {
+            if (_userDoc.exists) {
+              let sessionID = authCollect.doc(_userDoc.data().id).collection("status").doc(req.sessionID);
+              let authLog = authCollect.doc(_userDoc.data().id).collection("_log").doc(`signin-${req.sessionID}`);
+
+              sessionID.update({
+                uid: userDoc.id,
+                session: req.sessionID,
+                provider: req.user.provider,
+                facebook_id: req.user.id,
+                login_on: true,
+                date: req._startTime,
+                key_token: `${req._startAt[1]}`,
+                token: token,
+                remoteAddress: req._remoteAddress,
+                ...req.headers,
+                ..._geoRes.body
+              }).catch(function () {
+                sessionID.set({
+                  uid: userDoc.id,
+                  session: req.sessionID,
+                  provider: req.user.provider,
+                  facebook_id: req.user.id,
+                  login_on: true,
+                  date: req._startTime,
+                  key_token: `${req._startAt[1]}`,
+                  token: token,
+                  remoteAddress: req._remoteAddress,
+                  ...req.headers,
+                  ..._geoRes.body
+                })
+              });
+
+              authLog.update({
+                id: authLog.id,
+                uid: userDoc.id,
+                session: req.sessionID,
+                provider: req.user.provider,
+                facebook_id: req.user.id,
+                status: "signin",
+                date: req._startTime,
+                key_token: `${req._startAt[1]}`,
+                token: token,
+                _remoteAddress: req._remoteAddress,
+                _endTime: req._startTime,
+                _endAt: req._startAt,
+                ...req.headers,
+                ..._geoRes.body
+              }).catch(function () {
+                authLog.set({
+                  id: authLog.id,
+                  uid: userDoc.id,
+                  session: req.sessionID,
+                  provider: req.user.provider,
+                  facebook_id: req.user.id,
+                  status: "signin",
+                  date: req._startTime,
+                  key_token: `${req._startAt[1]}`,
+                  token: token,
+                  _remoteAddress: req._remoteAddress,
+                  _startTime: req._startTime,
+                  _startAt: req._startAt,
+                  ...req.headers,
+                  ..._geoRes.body
+                });
+              });
+
+
+
+            } else {
+              console.log("no uid");
+            }
+          })
+
+        } else {
+          // doc.data() will be undefined in this case
+          let userDoc = userCollect.doc();
+          userDoc.set({
+            id: userDoc.id,
+            name: req.user.displayName,
+            facebook_id: req.user.id
+          }).then(function () {
+            facebookUser.set({
+              uid: userDoc.id,
+              ...req.user
+            }).then(function () {
+              let sessionID = authCollect.doc(userDoc.id).collection("status").doc(req.sessionID);
+              let authLog = authCollect.doc(userDoc.id).collection("_log").doc(`signup-${req.sessionID}`);
+              sessionID.update({
+                uid: userDoc.id,
+                session: req.sessionID,
+                provider: req.user.provider,
+                facebook_id: req.user.id,
+                login_on: true,
+                date: req._startTime,
+                key_token: `${req._startAt[1]}`,
+                token: token,
+                remoteAddress: req._remoteAddress,
+                ...req.headers,
+                ..._geoRes.body
+              }).catch(function () {
+                sessionID.set({
+                  uid: userDoc.id,
+                  session: req.sessionID,
+                  provider: req.user.provider,
+                  facebook_id: req.user.id,
+                  login_on: true,
+                  date: req._startTime,
+                  key_token: `${req._startAt[1]}`,
+                  token: token,
+                  remoteAddress: req._remoteAddress,
+                  ...req.headers,
+                  ..._geoRes.body
+                });
+              });
+              authLog.update({
+                id: authLog.id,
+                uid: userDoc.id,
+                session: req.sessionID,
+                provider: req.user.provider,
+                facebook_id: req.user.id,
+                status: "signup",
+                date: req._startTime,
+                key_token: `${req._startAt[1]}`,
+                token: token,
+                _remoteAddress: req._remoteAddress,
+                _endTime: req._startTime,
+                _endAt: req._startAt,
+                ...req.headers,
+                ..._geoRes.body
+              }).catch(function () {
+                authLog.set({
+                  id: authLog.id,
+                  uid: userDoc.id,
+                  session: req.sessionID,
+                  provider: req.user.provider,
+                  facebook_id: req.user.id,
+                  status: "signup",
+                  date: req._startTime,
+                  key_token: `${req._startAt[1]}`,
+                  token: token,
+                  _remoteAddress: req._remoteAddress,
+                  _startTime: req._startTime,
+                  _startAt: req._startAt,
+                  ...req.headers,
+                  ..._geoRes.body
+                });
+              });
+
+              // arj.ok(res, true, "ok", { ...req.user })
+            })
+          })
+          console.log("No such document!");
+        }
+      })
+
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
     return next();
   }
 
 
   res.redirect('/api/example/facebook');
 }
+
+
 module.exports = router;
